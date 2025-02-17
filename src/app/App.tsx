b1dda9d73478bb400dc09f250e3c9725fece2d84
Daniel <daniel.saneusebio@fitfai.com>
2025-02-17 15:53:29 +0000

    feat: Update delete account instructions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,22 @@
-import { CssBaseline } from "@mui/material";
-import { Container } from "@mui/system";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Layout from "./c-components/Layout/Layout";
-import { NavBarButtons } from "./c-components/Layout/models/layoutProps.models";
-import DeleteAccountInstructions from "./components/DeleteAccountInstructions/DeleteAccountInstructions";
+import { CssBaseline } from '@mui/material';
+import { Container } from '@mui/system';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './c-components/Layout/Layout';
+import { NavBarButtons } from './c-components/Layout/models/layoutProps.models';
+import DeleteAccountInstructions from './components/DeleteAccountInstructions/DeleteAccountInstructions';
 
 function App() {
   const navBarInfo: NavBarButtons[] = [];
   return (
     <>
       <CssBaseline />
-      <Container disableGutters sx={{ minWidth: "100%" }}>
-        <Layout navBarButtons={navBarInfo} staticNavbar={true}>
+      <Container disableGutters sx={{ minWidth: '100%' }}>
+        <Layout
+          navBarButtons={navBarInfo}
+          navBarLogoUrl="https://fitfai.com/"
+          staticNavbar={true}
+        >
           <Routes>
             <Route path="/" element={<DeleteAccountInstructions />} />
             <Route path="*" element={<DeleteAccountInstructions />} />
--- src/app/components/DeleteAccountInstructions/DeleteAccountInstructions.tsx
+++ src/app/components/DeleteAccountInstructions/DeleteAccountInstructions.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import {
-  Container,
-  Typography,
-  List,
-  ListItem,
-  Link,
-  Box,
-  Grid
-} from '@mui/material';
+import { Container, Typography, Link, Box, Grid } from '@mui/material';
 
 const DeleteAccountInstructions = () => {
   return (
@@ -31,29 +22,77 @@ const DeleteAccountInstructions = () => {
           <Typography variant="h4" fontWeight="bold" gutterBottom>
             Account Deletion Instructions
           </Typography>
-          <Typography variant="body1" paragraph>
-            If you wish to delete your account and anonymize your data, follow
-            these simple steps:
-          </Typography>
-          <List sx={{ textAlign: 'left' }}>
-            <ListItem>1. Log in to your account on the app.</ListItem>
-            <ListItem>
-              2. Navigate to "Settings" (tap the icon in the top right corner of
-              the Home screen).
-            </ListItem>
-            <ListItem>3. Scroll down and click on "Delete Account".</ListItem>
-            <ListItem>
-              4. Your account will be permanently deleted, and any fitness data
-              will be anonymized.
-            </ListItem>
-          </List>
-          <Typography variant="body1" sx={{ mt: 2 }}>
-            If you have any questions, feel free to{' '}
-            <Link href="mailto:support@fitfai.com" underline="hover">
-              contact us
-            </Link>
-            .
-          </Typography>
+          <Box
+            sx={{
+              textAlign: 'left',
+              mt: 5
+            }}
+          >
+            <Typography variant="h5" fontWeight="bold" paragraph>
+              To delete your account
+            </Typography>
+            <Box sx={{ pl: 2 }}>
+              <Typography sx={{ mb: 2 }}>
+                1. Open Fitfai app and <b>Log in</b> to your account.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                2. Navigate to <b>Settings</b> (tap the icon in the top right
+                corner of the Home screen).
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                3. Scroll down and click on <b>Delete Account</b>.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                4. A message will appear asking if you're sure. Tap{' '}
+                <b>Delete</b> to permanently delete your account.
+              </Typography>
+            </Box>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              If you have any questions or require any assistance, feel free to
+              contact us at{' '}
+              <Link href="mailto:support@fitfai.com" underline="hover">
+                support@fitfai.com
+              </Link>
+              .
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              textAlign: 'left',
+              mt: 5
+            }}
+          >
+            <Typography variant="h6" fontWeight="bold" paragraph>
+              Deleting your account will:
+            </Typography>
+            <Box sx={{ pl: 2 }}>
+              <Typography sx={{ mb: 2 }}>
+                - Your account will be permanently deleted.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                - All personal data associated with your account will be erased.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                - Any fitness data, including workout history and progress, will
+                be anonymized.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                - You will no longer have access to any content, services, or
+                features associated with your account.
+              </Typography>
+              <Typography sx={{ mb: 2 }}>
+                - You will not be able to recover any deleted data once the
+                process is completed.
+              </Typography>
+            </Box>
+            <Typography variant="body1" sx={{ mt: 5 }}>
+              For more details on how we handle your data, please refer to our{' '}
+              <Link href="https://fitfai.com/privacy-policy" underline="hover">
+                Privacy Policy
+              </Link>
+              .
+            </Typography>
+          </Box>
         </Box>
       </Container>
     </Grid>
